416c10e09a1b6fc29089bdbb64bc9e408bf0bafa
oh930418 <oh930418@naver.com>
2017-04-21 18:00:55 +0900

    Update main.cpp

--- Projects/Omok game program/untitled2/main.cpp
+++ Projects/Omok game program/untitled2/main.cpp
@@ -11,23 +11,3 @@ int main(int argc, char *argv[])
 }
 
 
-
-/*
-
-
-
-                  if(pan[location[j][0]-1][location[j][1]-1] == 0){
-                      MAX_y_PC[num] = location[j][0]-1;
-                      MAX_x_PC[num] = location[j][1]-1;
-                      return dia1;
-                  }else if(pan[location[j][0]-stone_num][location[j][1]-stone_num] == 0){
-                      MAX_x_PC[num] = location[j][1]-stone_num;
-                      MAX_y_PC[num] = location[j][0]-stone_num;
-                      return dia1;
-                  }
-
-
- *
- *
- *
- * */
